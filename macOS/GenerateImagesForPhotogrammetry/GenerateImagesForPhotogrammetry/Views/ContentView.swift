6435cc5f87ba83717f41adf94410bc1af7928f57
kokiTakeda <bluewhitered123@gmail.com>
2023-01-15 00:25:42 +0900

    create new project

---
+++ macOS/GenerateImagesForPhotogrammetry/GenerateImagesForPhotogrammetry/Views/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GenerateImagesForPhotogrammetry
+//
+//  Created by 武田孝騎 on 2023/01/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
