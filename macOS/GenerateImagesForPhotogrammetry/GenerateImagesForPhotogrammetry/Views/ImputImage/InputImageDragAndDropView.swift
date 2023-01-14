7f185c0b52cc7df8f4e50411ce3b891fcf55daf3
kokiTakeda <bluewhitered123@gmail.com>
2023-01-15 02:18:42 +0900

    画像と動画でドラッグアンドドロップを導入。

---
+++ macOS/GenerateImagesForPhotogrammetry/GenerateImagesForPhotogrammetry/Views/ImputImage/InputImageDragAndDropView.swift
@@ -0,0 +1,56 @@
+//
+//  InputImageDragAndDropView.swift
+//  GenerateImagesForPhotogrammetry
+//
+//  Created by 武田孝騎 on 2023/01/15.
+//
+
+import SwiftUI
+
+struct InputImageDragAndDropView: View {
+    
+    @Binding var image: NSImage?
+        
+    var body: some View {
+        ZStack {
+            if image != nil {
+                Image(nsImage: image!)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } else {
+                Text("Drag and drop image file")
+                    .frame(width: 320)
+            }
+        }
+        .frame(height: 320)
+        .background(Color.black.opacity(0.5))
+        .cornerRadius(8)
+            
+        .onDrop(of: ["public.file-url"], isTargeted: nil, perform: handleOnDrop(providers:))
+    }
+        
+    private func handleOnDrop(providers: [NSItemProvider]) -> Bool {
+        if let item = providers.first {
+            item.loadItem(forTypeIdentifier: "public.file-url", options: nil) { (urlData, error) in
+                DispatchQueue.main.async {
+                    if let urlData = urlData as? Data {
+                        let url = NSURL(absoluteURLWithDataRepresentation: urlData, relativeTo: nil) as URL
+                        guard let image = NSImage(contentsOf: url) else {
+                            return
+                        }
+                        self.image = image
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+}
+
+struct InputImageView_Previews: PreviewProvider {
+    @State static var image: NSImage? = nil //NSImage()
+    static var previews: some View {
+        InputImageDragAndDropView(image: $image)
+    }
+}
---
+++ macOS/GenerateImagesForPhotogrammetry/GenerateImagesForPhotogrammetry/Views/ImputVideo/InputVideoDragAndDropView.swift
@@ -0,0 +1,57 @@
+//
+//  InputVideoDragAndDropView.swift
+//  GenerateImagesForPhotogrammetry
+//
+//  Created by 武田孝騎 on 2023/01/15.
+//
+
+import SwiftUI
+import AVKit
+
+struct InputVideoDragAndDropView: View {
+    
+    @Binding var videoUrl: NSURL?
+        
+    var body: some View {
+        ZStack {
+            if videoUrl != nil {
+//                Image(nsImage: filteredImage != nil ? filteredImage! : videoUrl!)
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fit)
+                VideoPlayer(player: AVPlayer(url: (videoUrl!) as URL))
+                    .frame(height: 320)
+            } else {
+                Text("Drag and drop video file")
+                    .frame(width: 320)
+            }
+        }
+        .frame(height: 320)
+        .background(Color.black.opacity(0.5))
+        .cornerRadius(8)
+        .padding(.trailing, 16.0)
+        .padding(.leading, 16.0)
+            
+        .onDrop(of: ["public.file-url"], isTargeted: nil, perform: handleOnDrop(providers:))
+    }
+        
+    private func handleOnDrop(providers: [NSItemProvider]) -> Bool {
+        if let item = providers.first {
+            item.loadItem(forTypeIdentifier: "public.file-url", options: nil) { (urlData, error) in
+                DispatchQueue.main.async {
+                    if let urlData = urlData as? Data {
+                        self.videoUrl = NSURL(absoluteURLWithDataRepresentation: urlData, relativeTo: nil)
+                    }
+                }
+            }
+            return true
+        }
+        return false
+    }
+}
+
+struct InputVideoDragAndDropView_Previews: PreviewProvider {
+    @State static var videoUrl: NSURL? = nil //NSImage()
+    static var previews: some View {
+        InputVideoDragAndDropView(videoUrl: $videoUrl)
+    }
+}
